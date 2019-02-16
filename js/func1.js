f17170426f7c89c3a55cd92a767f40693cc38be6
KSM06-02 <test@test.com>
2019-02-16 14:37:07 +0900

    this의 scope1 : global

---
+++ js/func1.js
@@ -0,0 +1,19 @@
+function person() {
+  this.arms = 2;
+  this.legs = 2;
+}
+
+// arms의 출력 결과는 무엇인가?
+// console.log(arms);
+
+// Person() 실행결과는 무엇인가? 그 이유는?
+console.log(person()); // undefined
+
+// arms의 출력 결과는 무엇인가?  여기서 사용된 this는 무엇인가?
+console.log(arms); // 2 -> person에서 생성된 global window.arms 참조
+
+/**
+ * this - 글로벌 this
+ *        부모 this
+ *        자기자신 this (new)
+ */
