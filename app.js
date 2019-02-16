54619ed2a984c5c95e9d89598163b40bd8b6316f
KSM06-02 <test@test.com>
2019-02-16 16:11:21 +0900

    function component

--- app.js
+++ app.js
@@ -3,14 +3,46 @@ const myTitleId = 'main-title';
 const title = 'My First React Element';
 const desc = 'This is a Description';
 
-// 선택 범위 : control - w
-const head = (
+// webstrom 범위 선택  : control - w
+// const head = (
+//   <header>
+//     <h1 id={myTitleId} title={myTitle}>111 {title}</h1>
+//     <p>{desc}</p>
+//   </header>
+// )
+
+// function component
+// 함수 컴포넌트를 **호출(생성이 아니다.)**하면 JSX형식에 맞춰진 ReactElement객체를 리턴
+const Header = (props) => (
   <header>
-    <h1 id={myTitleId} title={myTitle}>111 {title}</h1>
-    <p>{desc}</p>
+    <h1>Scoreboard</h1>
+    <span className="stats">Players: 1</span>
   </header>
-)
+);
 
-console.log(head);
+const Counter = (props) => (
+  <div className="counter">
+    <button className="counter-action">+</button>
+    <span className="counter-score">35</span>
+    <button className="counter-action">-</button>
+  </div>
+);
 
-ReactDOM.render( head, document.getElementById('root'));
+const Player = (props) => (
+  <div className="player">
+    <span className="player-name">song</span>
+    <Counter/>
+  </div>
+);
+
+const App = (props) => {
+  return (
+    <div className="scoreboard">
+      <Header/>
+      {/*play list*/}
+      <Player/>
+    </div>
+  )
+}
+
+ReactDOM.render( <App/>, document.getElementById('root'));
